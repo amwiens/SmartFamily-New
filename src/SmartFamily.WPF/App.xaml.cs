eeaaf124d3bdfac99429be7c0f14455a6db2565e
Aaron Wiens <amwiens@hotmail.com>
2020-06-14 21:30:18 -0500

    Added the WPF project.

---
+++ src/SmartFamily.WPF/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+
+using SmartFamily.WPF.Views;
+
+using System.Windows;
+
+namespace SmartFamily.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
